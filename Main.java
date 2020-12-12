f3884bfbd3a51420adde20c945150ff922047d9d
xavier niquet <niquet_xavier2002@yahoo.fr>
2020-12-12 17:04:53 +0100

    1

---
+++ Main.java
@@ -0,0 +1,25 @@
+package bullscows;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The secret code is prepared: ****.");
+        System.out.println("Turn 1. Answer:\n" +
+                "1234\n" +
+                "Grade: 1 cow.\n");
+        System.out.println("Turn 2. Answer:\n" +
+                "5678\n" +
+                "Grade: 3 cow.\n");
+        System.out.println("Turn 3. Answer:\n" +
+                "5679\n" +
+                "Grade: 3 cow.\n");
+        System.out.println("Turn 4. Answer:\n" +
+                "5674\n" +
+                "Grade: 4 cow.\n");
+        System.out.println("Turn 5. Answer:\n" +
+                "4567\n" +
+                "Grade: 4 bull.\n");
+        System.out.println("Congrats! The secret code is 4567");
+
+    }
+}
